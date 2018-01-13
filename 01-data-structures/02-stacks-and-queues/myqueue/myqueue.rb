882147e9e8937da7b6ab20389bc91ba3af58428f
Alex Erling <alex.erling@gmail.com>
2018-01-13 16:34:12 -0600

    Completed Sep Assignment 2

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -7,12 +7,32 @@ class MyQueue
     @head = @queue[0]
   end
 
+# DEF ENQUEUE(element)
+#   Add element to the end of the list
+# END DEF
+
   def enqueue(element)
+    @queue.insert(@queue.length, element)
+    @head = @queue.first
+    @tail = @queue.last
   end
 
+# DEF DEQUEUE
+#   SET temp to the first element
+#   Left-Shift the list by one entry
+#   RETURN temp
+# END DEF
+
+
   def dequeue
+    if !empty?
+      @queue.delete_at(@queue.length-1)
+    end
+    @tail = @queue.last
+    @head = @queue.first
   end
 
   def empty?
+    @queue.length == 0
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -6,12 +6,28 @@ class MyStack
     self.top = nil
   end
 
+# DEF PUSH(element)
+#   INCREMENT the stack position index by 1
+#   ASSIGN element to the location at the stack position index
+# END DEF
+
   def push(item)
+    @stack.unshift(item)
+    self.top = @stack[0]
   end
 
+# DEF POP
+#   ASSIGN the element at the stack position index to temp
+#   DECREMENT the stack position index by 1
+#   RETURN temp
+# END DEF
+
   def pop
+    self.top = @stack[1]
+    @stack.shift
   end
 
   def empty?
+    @stack.length == 0
   end
 end
